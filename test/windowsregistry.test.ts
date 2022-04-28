e7eccd72fe83e8c0d6f888041886d419bd4080c7
khkassem <khkassem@gmail.com>
2022-04-28 06:18:25 +0300

    change to lib/index.ts

--- test/windowsregistry.test.ts
+++ test/windowsregistry.test.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { GetStringRegKey } from '../dist/index';
+import { GetStringRegKey } from '../lib/index';
 import * as assert from 'assert';
 
 describe('Windows Registry Tests', () => {
