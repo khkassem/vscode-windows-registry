23545b43c0542aa2ef6cbb263e1e11a6ffabe420
khkassem <khkassem@gmail.com>
2022-04-28 06:01:50 +0300

    adding function DWORD export

--- lib/index.ts
+++ lib/index.ts
@@ -15,3 +15,12 @@ export function GetStringRegKey(hive: HKEY, path: string, name: string): string 
 	console.error('Could not initialize Windows Registry native node module.');
 	return undefined;
 }
+
+export function GetDWORDRegKey(hive: HKEY, path: string, name: string): string | undefined {
+	if (windowregistry) {
+		return windowregistry.GetDWORDRegKey(hive, path, name);
+	}
+
+	console.error('Could not initialize Windows Registry native node module.');
+	return undefined;
+}
